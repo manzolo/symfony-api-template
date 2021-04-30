27516c392321873f26040091b5109d9cbfa9fc36
Andrea Manzi <manzolo@libero.it>
2021-04-30 19:24:11 +0200

    Upgrade dependencies

---
+++ public/build/app.6779df78.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[143],{15177:()=>{"use strict";console.log("Hello Webpack Encore! Edit me in assets/js/app.js")}},s=>{"use strict";var e;e=15177,s(s.s=e)}]);
