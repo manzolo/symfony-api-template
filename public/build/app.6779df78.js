541644232bfd3409ce08cdf0d88884e8648a7bb0
Andrea Manzi <andrea.manzi@comune.fi.it>
2021-09-29 21:37:45 +0200

    upgrade dependencies

--- public/build/app.6779df78.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk=self.webpackChunk||[]).push([[143],{15177:()=>{"use strict";console.log("Hello Webpack Encore! Edit me in assets/js/app.js")}},s=>{"use strict";var e;e=15177,s(s.s=e)}]);
